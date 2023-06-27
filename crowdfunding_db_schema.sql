e43e1a6883eefd67ec21f84160a30e5f0e5a230a
azbennett <stephen.f.bennett@gmail.com>
2023-06-26 19:52:30 -0700

    updated schema import

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE Contacts;
+
 CREATE TABLE Contacts (
   contact_id INT PRIMARY KEY,
   first_name VARCHAR(50),
@@ -5,15 +7,38 @@ CREATE TABLE Contacts (
   email VARCHAR(255)
 );
 
+COPY Contacts FROM 'C:\Users\sbenn\Desktop\ASU\Classwork\git\Crowdfunding_ETL\output_data\contacts.csv' DELIMITER ',' CSV HEADER;
+--good
+SELECT * FROM Contacts;
+
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+DROP TABLE Category;
+
 CREATE TABLE Category (
   category_id VARCHAR(10) PRIMARY KEY,
   category VARCHAR(25)
 );
 
+COPY Category FROM 'C:\Users\sbenn\Desktop\ASU\Classwork\git\Crowdfunding_ETL\output_data\category.csv' DELIMITER ',' CSV HEADER;
+--good
+SELECT * FROM Category;
+
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+DROP TABLE Subcategory;
+
 CREATE TABLE Subcategory (
   subcategory_id VARCHAR(10) PRIMARY KEY,
   sub_category VARCHAR(25)
 );
+
+COPY Subcategory FROM 'C:\Users\sbenn\Desktop\ASU\Classwork\git\Crowdfunding_ETL\output_data\subcategory.csv' DELIMITER ',' CSV HEADER;
+--good
+SELECT * FROM Subcategory;
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+DROP TABLE Campaign;
 
 CREATE TABLE Campaign (
   cf_id INT PRIMARY KEY,
@@ -34,3 +59,7 @@ CREATE TABLE Campaign (
   FOREIGN KEY (category_id) REFERENCES Category(category_id),
   FOREIGN KEY (subcategory_id) REFERENCES Subcategory(subcategory_id)
 );
+
+COPY Campaign FROM 'C:\Users\sbenn\Desktop\ASU\Classwork\git\Crowdfunding_ETL\output_data\campaign.csv' DELIMITER ',' CSV HEADER;
+--good
+SELECT * FROM Campaign;
